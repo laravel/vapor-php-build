1943ff18b09dad0f9b5d832937151d3bdafce893
Joe Dixon <hello@joedixon.co.uk>
2023-05-23 12:10:58 +0100

    allow aws credentials to be loaded

--- publish.php
+++ publish.php
@@ -5,7 +5,7 @@ use Dotenv\Dotenv;
 
 require_once __DIR__.'/vendor/autoload.php';
 
-Dotenv::createImmutable(__DIR__)->safeLoad();
+Dotenv::createUnsafeImmutable(__DIR__)->safeLoad();
 
 $layers = [
     // Amazon Linux 1:
